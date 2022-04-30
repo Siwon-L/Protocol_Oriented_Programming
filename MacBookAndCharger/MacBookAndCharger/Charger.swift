123bbc17f3e6ef1e854697bccf32ab2c0dc9bdda
Red <seryprk@gmail.com>
2022-04-30 10:22:13 +0900

    feat: Charger 구현

--- MacBookAndCharger/MacBookAndCharger/Charger.swift
+++ MacBookAndCharger/MacBookAndCharger/Charger.swift
@@ -5,4 +5,13 @@
 //  Created by 박세리 on 2022/04/30.
 //
 
-import Foundation
+struct Charger: Chargeable {
+    var maximumWattPerHour: WattPerHour
+    
+    mutating func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour {
+        if maximumWattPerHour > chargeableWattPerHour {
+            maximumWattPerHour = chargeableWattPerHour
+        }
+        return maximumWattPerHour
+    }
+}
